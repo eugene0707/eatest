73b976ee51344edd8a586f99962ccba869cb0221
eugene0707 <eugene0707@gmail.com>
2015-10-19 02:59:19 +0300

    Fix for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,10 +24,6 @@
 
 //= require angular-app/app
 //= require_tree ./angular-app/templates
-//= require_tree ./angular-app/modules
-//= require_tree ./angular-app/filters
-//= require_tree ./angular-app/directives
-//= require_tree ./angular-app/models
 //= require_tree ./angular-app/services
 //= require_tree ./angular-app/controllers
 //= require angular-app/routes
