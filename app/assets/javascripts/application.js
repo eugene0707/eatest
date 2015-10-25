004b27c1b1cbaf79cbba0fd94f5973d22d5dd44a
eugene0707 <eugene0707@gmail.com>
2015-10-25 21:11:53 +0300

    Fix for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,8 @@
 //= require angular-sanitize/angular-sanitize
 //= require angular-loading-bar
 
+//= require angular-bootstrap/ui-bootstrap
 //= require angular-bootstrap/ui-bootstrap-tpls
-//= require angular-bootstrap/index
 //= require angular-ui-mask/dist/mask
 
 //= require bootstrap-sprockets
